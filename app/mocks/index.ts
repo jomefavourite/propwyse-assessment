f1627b98b31a87401ed0dde8b9980f0df3f41871
Jome Favourite <jfjomefavourite@gmail.com>
2025-06-22 19:02:00 +0100

    feat: setup mock

---
+++ app/mocks/index.ts
@@ -0,0 +1,8 @@
+if (typeof window === 'undefined') {
+  const { server } = require('./node');
+  server.listen();
+} else {
+  const { worker } = require('./browser');
+  worker.start();
+}
+export {};
